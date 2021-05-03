dd26ce40c3391cb32ab6f5857f8b1885dea3a689
igrekus <upload234@mail.ru>
2021-05-03 20:44:52 +0300

    FEAT: implement random guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number.");
+
+    let secret_num = rand::thread_rng().gen_range(1..101);
+
+    // println!("The secret num is: {}", secret_num);
+
+    loop {
+        println!("Input guess:");
+        let mut guess = String::new();
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => { println!("type a number"); continue; },
+        };
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("less"),
+            Ordering::Greater => println!("greater"),
+            Ordering::Equal => { println!("win"); break; },
+        }
+        
+        // println!("Your guess: {}", guess);
+    }
 }
